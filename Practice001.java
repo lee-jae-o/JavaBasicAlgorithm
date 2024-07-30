5c8cc02ca3be82912bcb59fa4e356e40b0abba44
lee-jae-o <127208195+lee-jae-o@users.noreply.github.com>
2024-07-30 17:31:16 +0900

    Add files via upload

---
+++ Practice001.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Practice001 {
+    public static void main(String[] args) {
+
+        Scanner cal = new Scanner(System.in);
+
+        System.out.print("A값 입력: ");
+        int A = cal.nextInt();
+
+        System.out.print("B값 입력: ");
+        int B = cal.nextInt();
+
+        System.out.println("A + B = " + (A+B) );
+        System.out.println("A - B = " + (A-B) );
+        System.out.println("A * B = " + (A*B) );
+        System.out.println("A / B = " + (A/B) );
+    }
+}
