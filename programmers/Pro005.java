ce800185d514f9ea84faed2e84e0c3c07c532313
lee-jae-o <127208195+lee-jae-o@users.noreply.github.com>
2024-08-03 17:42:24 +0900

    Add files via upload

---
+++ programmers/Pro005.java
@@ -0,0 +1,16 @@
+package programmers;
+
+import java.util.*;
+
+public class Pro005{
+    public int solution(int[] array){
+        int answer = 0;
+        String str = Arrays.toString(array);
+        for(int i = 0; i < str.length(); i++){
+            if(str.charAt(i) == '7'){
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
