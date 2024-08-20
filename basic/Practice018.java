ac4a75ea9ce5774f3d73749340ca1d9fff18dea1
lee-jae-o <127208195+lee-jae-o@users.noreply.github.com>
2024-08-20 13:43:48 +0900

    Add files via upload

---
+++ basic/Practice018.java
@@ -0,0 +1,16 @@
+package basic;
+
+import java.util.Scanner;
+
+public class Practice018 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String text = sc.next();
+
+        char textCtr = text.charAt(0);
+
+        System.out.println((int)textCtr);
+    }
+}
---
+++ basic/Practice019.java
@@ -0,0 +1,14 @@
+package basic;
+
+import java.util.Scanner;
+
+public class Practice019 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String ID = sc.next();
+
+        System.out.println(ID + "??!");
+    }
+}
