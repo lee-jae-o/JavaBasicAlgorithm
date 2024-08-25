8d482273d9a7dc5241c8b961118b938b2ea53e68
lee-jae-o <127208195+lee-jae-o@users.noreply.github.com>
2024-08-25 13:43:21 +0900

    Add files via upload

---
+++ basic/Practice021.java
@@ -0,0 +1,21 @@
+package basic;
+
+import java.util.Scanner;
+
+public class Practice021 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int fac = 1;
+
+        for (int i=1; i<=N; i++) {
+            fac *= i;
+        }
+
+        System.out.println(fac);
+    }
+}
