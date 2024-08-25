d1cd40b96a10d56e853718ec22d83096d4dac4dd
lee-jae-o <127208195+lee-jae-o@users.noreply.github.com>
2024-08-25 13:43:11 +0900

    Add files via upload

---
+++ basic/Practice020.java
@@ -0,0 +1,16 @@
+package basic;
+
+import java.util.Scanner;
+
+public class Practice020 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int year = sc.nextInt();
+
+        int a = 543;
+
+        System.out.println(year - a);
+    }
+}
