2551c5efa5b28a405b6d3438811dbd1698c622e1
lee-jae-o <127208195+lee-jae-o@users.noreply.github.com>
2024-09-07 14:01:55 +0900

    Add files via upload

---
+++ programmers/Pro029.java
@@ -0,0 +1,20 @@
+package programmers;
+
+import java.util.Arrays;
+public class Pro029 {
+    public int solution(String before, String after) {
+        int answer=-1;
+        char[] bef=before.toCharArray();
+        char[] aft=after.toCharArray();
+
+        Arrays.sort(bef);
+        Arrays.sort(aft);
+
+        if(Arrays.equals(bef, aft)) {
+            answer=1;
+        } else {
+            answer=0;
+        }
+        return answer;
+    }
+}
