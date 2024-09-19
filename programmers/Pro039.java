8d4b6e55236a6672c12681b1ffdfcca5eefde33d
lee-jae-o <127208195+lee-jae-o@users.noreply.github.com>
2024-09-19 15:56:30 +0900

    Add files via upload

---
+++ programmers/Pro039.java
@@ -0,0 +1,17 @@
+package programmers;
+/*** 외계어 사전 ***/
+public class Pro039 {
+    public int solution(String[] spell, String[] dic) {
+        int answer = 2; int index = 0;
+
+        for(int i = 0; i < dic.length; i++) {
+            index = 0;
+            for(int j = 0; j < spell.length; j++) {
+                if(dic[i].contains(spell[j])) index++;
+                if(index == spell.length) answer = 1;
+            }
+        }
+
+        return answer;
+    }
+}
